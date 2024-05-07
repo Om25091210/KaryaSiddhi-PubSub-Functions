6550f989ed3f3326f9b0478b5a0430bdca421f74
Omkumar <74484315+Om25091210@users.noreply.github.com>
2024-05-07 12:11:53 +0530

    Fixed timing trigger according to UTC.

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const SERVER_URL="https://project-gov-backend.vercel.app/";
 admin.initializeApp(functions.config().firebase);
 
-exports.checkNode = functions.pubsub.schedule("30 21  * * *")
+exports.checkNode = functions.pubsub.schedule("0 15 * * *")
     .onRun( async (context) => {
       const _d = new Date();
       const utc = _d.getTime();
@@ -13,16 +13,16 @@ exports.checkNode = functions.pubsub.schedule("30 21  * * *")
       const yyyy = nd.getFullYear();
       const date = dd+"-"+mm+"-"+yyyy;
       console.log("Activated", date, SERVER_URL);
-        //   const url = `https://152b-2409-4081-8719-9fa0-a10b-2d5a-6d0-1337.ngrok-free.app/fcm/push`;
-        //   const authOptions = {
-        //       method: "POST",
-        //       headers: {
-        //           "Content-Type": "application/json"
-        //       },
-        //   };
-        //   const response = await fetch(url, authOptions);
-        //   const code = response.status;
-        //   const result = await response.json();
-        //   return { code, result };
-        return null;
+      const url = `${SERVER_URL}pubSub/trigger`;
+      const authOptions = {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body:JSON.stringify(date),
+      };
+      const response = await fetch(url, authOptions);
+      const code = response.status;
+      const result = await response.json();
+      return { code, result };
     });
